4f40b1c5af8ced49970eadfb6b3f7fdddec4c9b9
Ryan Tablada <ryan.tablada@gmail.com>
2013-10-15 18:26:23 -0500

    Fixed Wrapper to be much more efficient

--- src/Hugofirth/Mailchimp/MailchimpWrapper.php
+++ src/Hugofirth/Mailchimp/MailchimpWrapper.php
@@ -22,126 +22,18 @@ class MailchimpWrapper {
 		$this->mc = $mc;
 	}
 
-	//Accessors for public member objects beyond this point
-
 	/**
-	 * Get Mailchimp_Folders object
+	 * Accessor for all of the Mailchimp instance's properties as methods
 	 *
-	 * @return Mailchimp_Folders
+	 * @param  string $name
+	 * @param  array $arguments
+	 * @return mixed
 	 */
-	public function folders()
+	public function __call($name, $arguments)
 	{
-		return $this->mc->folders;
-	}
-
-	/**
-	 * Get Mailchimp_Templates object
-	 *
-	 * @return Mailchimp_Templates
-	 */
-	public function templates()
-	{
-		return $this->mc->templates;
-	}
-
-	/**
-	 * Get Mailchimp_Users object
-	 *
-	 * @return Mailchimp_Users
-	 */
-	public function users()
-	{
-		return $this->mc->users;
-	}
-
-	/**
-	 * Get Mailchimp_Helper object
-	 *
-	 * @return Mailchimp_Helper
-	 */
-	public function helper()
-	{
-		return $this->mc->helper;
-	}
-
-	/**
-	 * Get Mailchimp_Mobile object
-	 *
-	 * @return Mailchimp_Mobile
-	 */
-	public function mobile()
-	{
-		return $this->mc->mobile;
-	}
-
-	/**
-	 * Get Mailchimp_Ecomm object
-	 *
-	 * @return Mailchimp_Ecomm
-	 */
-	public function ecomm()
-	{
-		return $this->mc->ecomm;
-	}
-
-	/**
-	 * Get Mailchimp_Neapolitan object
-	 *
-	 * @return Mailchimp_Neapolitan
-	 */
-	public function neapolitan()
-	{
-		return $this->mc->neapolitan;
-	}
-
-	/**
-	 * Get Mailchimp_Lists object
-	 *
-	 * @return Mailchimp_Lists
-	 */
-	public function lists()
-	{
-		return $this->mc->lists;
-	}
-
-	/**
-	 * Get Mailchimp_Campaigns object
-	 *
-	 * @return Mailchimp_Campaigns
-	 */
-	public function campaigns()
-	{
-		return $this->mc->campaigns;
-	}
-
-	/**
-	 * Get Mailchimp_Vip object
-	 *
-	 * @return Mailchimp_Vip
-	 */
-	public function vip()
-	{
-		return $this->mc->vip;
-	}
-
-	/**
-	 * Get Mailchimp_Reports object
-	 *
-	 * @return Mailchimp_Reports
-	 */
-	public function reports()
-	{
-		return $this->mc->reports;
-	}
-
-	/**
-	 * Get Mailchimp_Gallery object
-	 *
-	 * @return Mailchimp_Gallery
-	 */
-	public function gallery()
-	{
-		return $this->mc->gallery;
+		if (isset($this->mc->{$name})) {
+			return $this->mc->{$name};
+		}
 	}
 
 }
